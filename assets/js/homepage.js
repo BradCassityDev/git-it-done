8b08223def1efff1c503ad25c8bed7ce44f11267
Brad Cassity <bradley.cassity@gmail.com>
2020-09-04 16:41:03 -0600

    Completed the feature

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,6 +2,21 @@ var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
+var languageButtonsEl = document.querySelector("#language-buttons");
+
+var getFeaturedRepos = function(language) {
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";  
+    fetch(apiUrl)
+    .then(function(response) {
+        if(response.ok) {
+            response.json().then(function(data) {
+                displayRepos(data.items, language);
+            });
+        } else {
+            alert("Error: " + response.statusText);
+        }
+    });
+};
 
 var displayRepos = function(repos, searchTerm) {
     repoContainerEl.textContent = "";
@@ -85,4 +100,16 @@ var formSubmitHandler = function(event) {
     }
 };
 
+var buttonClickHandler = function(event) {
+    event.preventDefault();
+
+    var language = event.target.getAttribute("data-language");
+   
+    if(language) {
+        getFeaturedRepos(language);
+        repoContainerEl.textContent = "";
+    }
+}
+
 userFormEl.addEventListener("submit", formSubmitHandler);
+languageButtonsEl.addEventListener("click", buttonClickHandler);
